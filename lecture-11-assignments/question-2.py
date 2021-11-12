e1c8006e30497a8b853b6282d8bbc5a181caab90
Shizler <youremail@domain.com>
2021-11-12 12:59:47 +0300

    adding comments

--- lecture-11-assignments/question-2.py
+++ lecture-11-assignments/question-2.py
@@ -1,3 +1,8 @@
+"""
+Write sumnum(num1, num2) that returns the sum of numbers from num1 to num2 (both 
+inclusive). Call this function as shown below and print the result.
+"""
+
 def sumnum(num1, num2):
     # option 1: (n / 2)(first number + last number) = sum -> (101/2)(50 + 100)
     # option 2: for loop
--- lecture-11-assignments/question-3.py
+++ lecture-11-assignments/question-3.py
@@ -1,3 +1,9 @@
+"""
+Generalize the above function further and write sumnum(num1, num2, k) , such that it returns the 
+sum of given k th power of numbers from num1 to num2. For instance, if k=1 sum of numbers, if 
+k=2 sum of squares of numbers, if k=0.5 sum of square roots of numbers, etc. 
+"""
+
 def sumnum(num1, num2, k):
     total = 0
     for num in range(num1,num2+1):
